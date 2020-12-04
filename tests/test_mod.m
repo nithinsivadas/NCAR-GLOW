3214e33a1d740b2443de0e5e172a3649405b21fc
Nithin Sivadas <nithin@bu.edu>
2020-12-04 21:43:50 +0530

    relaxing the tolerance of output

--- tests/test_mod.m
+++ tests/test_mod.m
@@ -19,7 +19,7 @@ iono = glow(time, glat, glon, f107a, f107, f107p, Ap, Q, Echar, Nbins);
 
 assert(abs(iono.alt(i)  -  100.7) < 0.1, ['alt_km: expected 100.7, but got: ', num2str(iono.alt(i))])
 assert(abs(iono.Tn(i)  - 187.) < 0.01, ['Tn: expected 187., but got: ', num2str(iono.Tn(i))])
-assert(abs(iono.A5577(i) - 87.71) < 0.0001,   ['A5577: expected 87.71, but got: ', num2str(iono.A5577(i))])
+assert(abs(iono.A5577(i) - 87.71) < 0.01,   ['A5577: expected 87.71, but got: ', num2str(iono.A5577(i))])
 assert(abs(iono.totalIonizationRate(i) - 820.) < 0.01, ['ionrate: expected 820. but got: ', num2str(iono.totalIonizationRate(i))])
 assert(abs(iono.hall(i) - 1.1100e-4) < 1e-7, ['hall: expected 1.1100e-4 but got: ', num2str(iono.hall(i))])
 
